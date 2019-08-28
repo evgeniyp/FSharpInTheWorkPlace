44b55ccff72137484c8a5dc31c0860c6e946c73c
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2019-08-28 10:08:05 +0300

    Completed project;

---
+++ Cards.Tests/Dealing.fs
@@ -0,0 +1,26 @@
+﻿module FSharp.TV.Cards.Tests
+
+open NUnit.Framework
+open FSharp.TV.Cards
+
+[<Test>]
+let ``Should return 51 cards when dealing a carc from a new deck`` () =
+    // Arrange
+    let expected = 51
+    let deck = shuffle newDeck 1
+    // Act
+    let rem, _ = dealCard deck
+    let actual = rem |> shuffledDeckSize
+    // Assert
+    Assert.AreEqual(expected, actual, "There were not 51 cards in the deck, after dealing")
+
+[<Test>]
+let `` Should return 0 cards when dealing a card from a delpeted deck`` () =
+    // Arrange
+    let expected = 0
+    let deck = shuffle [] 1
+    // Act
+    let rem, _ = dealCard deck
+    let actual = rem |> shuffledDeckSize
+    // Assert
+    Assert.AreEqual(expected, actual, "There are still cards in the deck")
---
+++ Cards/Cards.fs
@@ -0,0 +1,30 @@
+﻿module FSharp.TV.Cards
+
+type Suit = | Clubs | Diamonds | Hearts | Spades
+type Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+type Card = Card of Suit*Rank
+type Deck = Card list
+type ShuffledDeck = ShuffledDeck of Deck
+type Deal = ShuffledDeck -> ShuffledDeck*Card option
+
+let allSuits = [ Clubs; Diamonds; Hearts; Spades ]
+let allRanks = [ Two ; Three ; Four ; Five ; Six ; Seven ; Eight ; Nine ; Ten ; Jack ; Queen ; King ; Ace ]
+
+let newDeck : Deck =
+    [ for suit in allSuits do
+        for rank in allRanks do
+            yield Card (suit, rank) ]
+
+let dealImpl deck =
+    match deck with
+    | ShuffledDeck(top::rem) -> ShuffledDeck rem, Some top
+    | ShuffledDeck [] -> ShuffledDeck [], None
+
+let dealCard : Deal = dealImpl
+
+let shuffle deck seed =
+    let rnd = System.Random(seed)
+    deck |> List.sortBy (fun _ -> rnd.Next()) |> ShuffledDeck
+
+let shuffledDeckSize (ShuffledDeck deck) =
+    List.length deck
